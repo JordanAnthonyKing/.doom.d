84f9027defe18b61819f8f89a8d655d5e33969f4
Jordan Cross <jordan@jordanking.dev>
2021-04-13 14:17:47 +0100

    initial commit

---
+++ config.el
@@ -0,0 +1,157 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Jordan King"
+      user-mail-address "jordan@jordanking.dev")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Source Han Code JP" :size 13 :weight 'normal))
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/documents/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+:
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(after! evil
+  (map! :leader
+        :desc "M-x" "SPC" #'execute-extended-command)
+
+  ;; TODO: Doesn't seem to do anything
+  ;; Just experimenting for now
+  (scroll-on-jump-advice-add evil-undo)
+  (scroll-on-jump-advice-add evil-redo)
+  (scroll-on-jump-advice-add evil-jump-item)
+  (scroll-on-jump-advice-add evil-ex-search-next)
+  (scroll-on-jump-advice-add evil-ex-search-previous)
+  (scroll-on-jump-advice-add evil-forward-paragraph)
+  (scroll-on-jump-advice-add evil-backward-paragraph)
+  (scroll-on-jump-advice-add evil-goto-first-line)
+
+  ;; Actions that themselves scroll.
+  (scroll-on-jump-advice-add better-jumper-jump-forward)
+  (scroll-on-jump-advice-add better-jumper-jump-backward)
+  (scroll-on-jump-with-scroll-advice-add evil-goto-line)
+  (scroll-on-jump-with-scroll-advice-add evil-scroll-down)
+  (scroll-on-jump-with-scroll-advice-add evil-scroll-up)
+  (scroll-on-jump-with-scroll-advice-add evil-scroll-line-to-center)
+  (scroll-on-jump-with-scroll-advice-add evil-scroll-line-to-top)
+  (scroll-on-jump-with-scroll-advice-add evil-scroll-line-to-bottom))
+;; (global-set-key [remap evil-jump-forward]  #'better-jumper-jump-forward)
+
+
+(map! "C-x C-j" #'skk-mode)
+
+(defun my/filter-items (orig-fun item)
+  (or (funcall orig-fun item)
+      (eq (gethash "kind" item) 13)
+      (eq (gethash "kind" item) 14)
+      (eq (gethash "kind" item) 15)
+      (eq (gethash "kind" item) 16)
+      (eq (gethash "kind" item) 17)
+      (eq (gethash "kind" item) 18)
+      (eq (gethash "kind" item) 19)
+      (eq (gethash "kind" item) 20)
+      (eq (gethash "kind" item) 21)
+      (eq (gethash "kind" item) 22)
+      (eq (gethash "kind" item) 23)
+      (eq (gethash "kind" item) 24)
+      (eq (gethash "kind" item) 25)
+      (eq (gethash "kind" item) 26)))
+
+(after! lsp-mode
+  (setq lsp-auto-execute-action nil
+                                        ; lsp-enable-imenu 1
+                                        ; lsp-imenu-index-symbol-kinds '(Method Property Field Constructor Enum Interface Event Struct)
+                                        ; lsp-imenu-index-symbol-kinds '(Miscellaneous)
+        lsp-clients-typescript-log-verbosity "debug"
+                                        ; Could be great for csharp
+                                        ; lsp-headerline-breadcrumb-enable 1
+                                        ; lsp-headerline-breadcrumb-segments '(project file symbols)
+        lsp-clients-typescript-plugins
+        (vector
+         (list :name "typescript-tslint-plugin"
+               :location "/usr/lib/node_modules/typescript-tslint-plugin/"))))
+;; :location "~/.emacs.d/.local/npm/node_modules/typescript-tslint-plugin/"))))
+
+(after! lsp-ui
+  (setq lsp-ui-sideline-show-code-actions nil
+        lsp-ui-sideline-show-symbol nil
+        lsp-ui-sideline-diagnostic-max-lines 10
+        lsp-ui-sideline-diagnostic-max-line-length 75))
+
+(after! doom-modeline
+  (setq doom-modeline-bar-width 2
+        doom-modeline-buffer-file-name-style 'buffer-name
+        doom-modeline-indent-info nil
+        doom-modeline-buffer-encoding nil
+        doom-modeline-vcs-max-length 20
+        doom-modeline-buffer-modification-icon nil))
+
+(after! treemacs
+  (setq treemacs-width 50
+        treemacs-recenter-distance 0.05
+        treemacs-recenter-after-file-follow 'on-distance)
+  (treemacs-follow-mode 1)
+  (treemacs-fringe-indicator-mode 1))
+
+(add-hook! treemacs-mode
+  (treemacs-load-theme "doom-colors"))
+
+;; TODO: Automatically open company on . if not already
+(after! company
+  (setq company-minimum-prefix-length 1))
+
+;; TODO: Make this purely idle-delay based instead
+(after! flycheck
+  (setq flycheck-check-syntax-automatically
+        '(save mode-enabled idle-buffer-switch new-line))
+  (map! :leader
+        (:prefix-map ("c" . "code")
+         :desc "Next error"     "n" #'flycheck-next-error
+         :desc "Previous error" "p" #'flycheck-previous-error)))
+
+;; DOOG
+(setq fancy-splash-image "~/.doom.d/doog2.png")
+
+(add-hook! +doom-dashboard-mode
+  (setq +doom-dashboard-banner-padding '(0 . 2)))
+
+;; (good-scroll-mode 1)
